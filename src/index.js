ee137e71cd4bdd178b03291b05c56cd5a2b06737
kimyunje <stoneaxe002@gmail.com>
2018-11-16 19:16:25 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import '@babel/polyfill' // 이 라인을 지우지 말아주세요!
 
 const templates = {
+
 }
 
 const rootEl = document.querySelector('.root')
@@ -13,14 +14,45 @@ const rootEl = document.querySelector('.root')
 // 5. 이벤트 리스너 등록하기
 // 6. 템플릿을 문서에 삽입
 
-// 할일 목록
+// 할일 목록-연제
 const CalenderEl = document.querySelector('.calender')
 CalenderEl.addEventListener('click', e => {
-  console.log(CalenderEl)
   e.preventDefault();
-  alert("test")
   goTodoList();
- })
- function goTodoList(){
+})
+function goTodoList(){
   window.open("https://nifty-wiles-1e30e5.netlify.com/", '_blank');
- }
+}
+// 팀원 소개 -연제
+const infoEl = document.querySelector('.emergency-box')
+const teamInfo = document.querySelector('.team-info')
+infoEl.addEventListener('click', e=>{
+  e.preventDefault();
+  // 1. 템플릿 복사
+  // 2. 요소 선택
+  // const teamInfo = document.querySelector('.team-info')
+  // 3. 필요한 데이터 불러오기
+  // 4. 내용 채우기
+  // teamInfo.classList.add('active')
+
+  teamInfo.style.display="block";
+  // 5. 이벤트 리스너 등록하기
+  // 6. 템플릿을 문서에 삽입
+})
+
+const deleteEl = document.querySelector('.delete-btn')
+
+
+deleteEl.addEventListener('click', e=>{
+  e.preventDefault();
+  // alert('삭제')
+  // 1. 템플릿 복사
+  // 2. 요소 선택
+  // 3. 필요한 데이터 불러오기
+  // 4. 내용 채우기
+  console.log('눌림')
+  // teamInfo.classList.remove('active')
+  teamInfo.style.display="none";
+  // 5. 이벤트 리스너 등록하기
+  // 6. 템플릿을 문서에 삽입
+})
